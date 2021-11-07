fbd2138000ea202f7f17291a14de6bc788594d7f
Namrata-art <59997637+Namrata-art@users.noreply.github.com>
2021-11-08 00:49:19 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+def app(df):
+  st.markdown("<p style='color:brown;font-family:cursive;font-size:35px'>Iris Flower Species Prediction App",unsafe_allow_html = True)
+
+  st.text("""
+    This web app allows a user to predict the type of iris flower.
+  """
+  )
+
+  st.header("View Data") 
+  #with st.expander("View Raw Dataset"):
+  if st.checkbox("View Raw Dataset"):
+    st.dataframe(df)
+    
+
+  if st.checkbox("Show summary"):
+    st.table(df.describe())
+
