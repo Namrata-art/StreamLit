2d0a6ce879dca773094b8811b3eb91cacb44c917
Namrata-art <59997637+Namrata-art@users.noreply.github.com>
2021-11-08 01:05:19 +0530

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def app(df):
-  st.markdown("<p style='color:brown;font-family:cursive;font-size:35px'>Iris Flower Species Prediction App",unsafe_allow_html = True)
+  st.markdown("<p style='color:brown;font-family:cursive;font-size:35px'>Iris Flower Species Data",unsafe_allow_html = True)
 
   st.text("""
     This web app allows a user to predict the type of iris flower.
